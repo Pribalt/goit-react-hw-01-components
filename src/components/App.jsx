896a9acc87fbf7b24152b4af7ad3e3945142f16f
Pribalt <vitalisvitalik@gmail.com>
2023-02-17 22:12:02 +0200

    add jsx file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import user from 'data.json/user.json';
+import data from 'data.json/data.json';
+import friends from 'data.json/friends.json';
+import transactions from 'data.json/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +16,13 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile user={user} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
